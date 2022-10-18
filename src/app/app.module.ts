661973554226934bfeafe55cf07fd34f05bca9fe
unknown <msegantine8@gmail.com>
2022-10-18 19:58:17 -0300

    Initial files for my blog made in Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing-module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AllArticlesComponent } from './pages/all-articles/all-articles.component';
+import { PortifolioComponent } from './pages/portifolio/portifolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AllArticlesComponent,
+    PortifolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/all-articles/all-articles.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllArticlesComponent } from './all-articles.component';
+
+describe('AllArticlesComponent', () => {
+  let component: AllArticlesComponent;
+  let fixture: ComponentFixture<AllArticlesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AllArticlesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AllArticlesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
