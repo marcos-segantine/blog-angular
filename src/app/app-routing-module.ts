f50ba1dae2fbadff148361edbcb80259aad8e34d
unknown <msegantine8@gmail.com>
2022-10-23 15:30:04 -0300

    Add Tecnologies component on the All Articles page to filter the articles

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from "./pages/home/home.component";
-import { AboutComponent } from "./pages/about/about.component";
-import { AllArticlesComponent } from "./pages/all-articles/all-articles.component";
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AllArticlesComponent } from './pages/all-articles/all-articles.component';
 
 const routes: Routes = [
-    {path: '', component: HomeComponent },
-    {path: 'all-articles', component: AllArticlesComponent }
-]
+  { path: '', component: HomeComponent },
+  { path: 'all-articles', component: AllArticlesComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-    declarations: [],
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
 export class AppRoutingModule {}
